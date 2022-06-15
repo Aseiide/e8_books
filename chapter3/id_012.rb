13b8c4c1156954b13e86388f0247d16c2af0576a
Aseiide <eiseiide@gmail.com>
2022-06-15 14:23:32 +0900

    ライブラリを使った素数判定

--- chapter3/id_012.rb
+++ chapter3/id_012.rb
@@ -1,17 +1,4 @@
+require 'prime'
+
 n = gets.to_i
-
-def prime?(n)
-  if n == 1
-    return false
-  end
-  2.upto(Math.sqrt(n)) do |i|
-    return false if n % i == 0
-  end
-  true
-end
-
-if prime?(n)
-  puts 'Yes'
-else
-  puts 'No'
-end
+puts n.prime? ? 'Yes' : 'No'
